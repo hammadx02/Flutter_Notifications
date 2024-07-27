cb4069ada4f01285a06cb44c168794b8775e67d3
hammadx02 <hammadaali02@gmail.com>
2024-07-27 16:44:13 +0500

    noti screen updated

--- lib/screens/notifications_screen.dart
+++ lib/screens/notifications_screen.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 
 class NotificationsScreen extends StatelessWidget {
@@ -5,12 +6,27 @@ class NotificationsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    // get the notification message and display on the screen
+    final message = ModalRoute.of(context)!.settings.arguments as RemoteMessage;
     return Scaffold(
       appBar: AppBar(
         title: const Text(
           'Notification Screen',
         ),
         centerTitle: true,
+      ),
+      body: Column(
+        children: [
+          Text(
+            message.notification!.title.toString(),
+          ),
+           Text(
+            message.notification!.body.toString(),
+          ),
+           Text(
+            message.data.toString(),
+          ),
+        ],
       ),
     );
   }
