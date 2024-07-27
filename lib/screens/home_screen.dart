f448e4701bcb34bfc712eff1cd51b2ee857da7bb
hammadx02 <hammadaali02@gmail.com>
2024-07-27 16:30:39 +0500

    notification screen added

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -5,6 +5,11 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return  Scaffold(appBar: AppBar(
+        title: const Text(
+          'Home Screen',
+        ),
+        centerTitle: true,
+      ),);
   }
 }
